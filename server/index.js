40cf410daaebb0ba5f09d540973beed0229f4256
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-07-26 20:00:02 -0500

    Add transiction api

--- server/index.js
+++ server/index.js
@@ -2,11 +2,25 @@ const express = require('express')
 const cors = require('cors')
 const pool = require('./db')
 const app = express()
-const PORT = 5432
+const PORT = 5000
 
 app.use(cors())
 app.use(express.json())
 
 app.listen(PORT, () => {
   console.log('Server has started on port ' + PORT)
+})
+
+// create a transaction
+app.post('/transactions', async (req, res) => {
+  try {
+    const { item, price, record_time } = req.body
+    const newTrans = await pool.query(
+      'INSERT INTO transactions (item, price, record_time) VALUES ($1, $2, $3) RETURNING *',
+      [item, price, record_time]
+    )
+    res.json(newTrans.rows[0])
+  } catch (error) {
+    console.log(error.message)
+  }
 })
