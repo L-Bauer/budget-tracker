aa49fa399a29d6ccdababb9c2afc8dcc7cdaee1c
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-07-31 12:32:16 -0500

    Change record_time to transaction_date
    Updated client input for the change in date.

--- client/src/components/InputTrans.js
+++ client/src/components/InputTrans.js
@@ -2,14 +2,14 @@ import React, { Fragment, useState } from "react"
 
 const InputTransaction = () => {
 
+  const [transaction_date, setTransaction_date] = useState("")
   const [item, setItem] = useState("")
   const [price, setPrice] = useState("")
 
   const onSubmitForm = async e => {
     e.preventDefault()
     try {
-      const time = "2023-07-28T05:00:00.000Z"
-      const body = {item, price, time}
+      const body = {transaction_date, item, price}
       const response = await fetch("http://localhost:5000/transactions", {
         method: "POST",
         headers: { "Content-Type": "application/json"},
@@ -25,6 +25,7 @@ const InputTransaction = () => {
     <Fragment>
       <h1 className="text-center mt-5">Expanse Tracker List</h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input type="date" className="form-control" value={transaction_date} onChange={e => setTransaction_date(e.target.value)}/>
         <input type="text" className="form-control" value={item} onChange={e => setItem(e.target.value)} placeholder="Item"/>
         <input type="text" className="form-control" value={price} onChange={e => setPrice(e.target.value)} placeholder="Price"/>
         <button className="btn btn-success">Add</button>
