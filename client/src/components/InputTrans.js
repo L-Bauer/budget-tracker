0102d3f17d7d3542a3f6a374754604c1cfa7ace7
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-07-31 15:02:40 -0500

    Change sizing and spacing

--- client/src/components/InputTrans.js
+++ client/src/components/InputTrans.js
@@ -10,7 +10,7 @@ const InputTransaction = () => {
     e.preventDefault()
     try {
       const body = {transaction_date, item, price}
-      const response = await fetch("http://localhost:5000/transactions", {
+      await fetch("http://localhost:5000/transactions", {
         method: "POST",
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify(body)
@@ -18,7 +18,7 @@ const InputTransaction = () => {
       setTransaction_date("")
       setItem("")
       setPrice("")
-      console.log(response)
+      window.location = "/"
     } catch (err) {
       console.error(err.message)
     }
@@ -28,13 +28,13 @@ const InputTransaction = () => {
     <Fragment>
       <h1 className="text-center mt-5">Expanse Tracker List</h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-        <input type="date" className="form-control" value={transaction_date}
+        <input type="date" className="form-control  m-2" value={transaction_date}
           onChange={e => setTransaction_date(e.target.value)}/>
-        <input type="text" className="form-control" value={item}
+        <input type="text" className="form-control m-2" value={item}
          onChange={e => setItem(e.target.value)} placeholder="Item"/>
-        <input type="text" className="form-control" value={price}
+        <input type="text" className="form-control m-2" value={price}
          onChange={e => setPrice(e.target.value)} placeholder="Price"/>
-        <button className="btn btn-success">Add</button>
+        <button className="btn btn-success w-75 h-85 m-2">Add</button>
       </form>
     </Fragment>
   )
