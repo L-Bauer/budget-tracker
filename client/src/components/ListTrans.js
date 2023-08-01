c505fdd3c2474cfea43e283265365594e8f7c100
Lucas Bauer <lucas.d.bauer95@gmail.com>
2023-08-01 14:23:49 -0500

    Create a list of transactions on webpage

--- client/src/components/ListTrans.js
+++ client/src/components/ListTrans.js
@@ -1,7 +1,25 @@
 
 import React, { Fragment, useEffect, useState } from "react";
+import {format} from 'date-fns';
 
 const ListTransactions = () => {
+  const [transactions, setTransactions] = useState([])
+
+  const getTransactions = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/transactions")
+      const transactionsData = await response.json()
+
+      setTransactions(transactionsData)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getTransactions()
+  }, [])
+
   return <Fragment>
     {" "}
     <table className="table table-striped table-hover text-center mt-5">
@@ -15,21 +33,15 @@ const ListTransactions = () => {
         </tr>
       </thead>
       <tbody>
-        {/*<tr>
-          <td>John</td>
-          <td>Doe</td>
-          <td>john@example.com</td>
-        </tr>
-        <tr>
-          <td>Mary</td>
-          <td>Moe</td>
-          <td>mary@example.com</td>
-        </tr>
-        <tr>
-          <td>July</td>
-          <td>Dooley</td>
-          <td>july@example.com</td>
-        </tr> */}
+        {transactions.map(transaction => (
+          <tr key={transaction.trans_id}>
+            <td>{format(new Date(transaction.transaction_date), 'MM/dd/yyyy')}</td>
+            <td>{transaction.item}</td>
+            <td>{transaction.price}</td>
+            <td>Edit</td>
+            <td>Delete</td>
+          </tr>
+        ))}
       </tbody>
     </table>
 </Fragment>
