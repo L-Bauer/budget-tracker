19e908800975bb8753227dfb602bc21ef557e0e1
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-08-15 07:23:51 -0500

    Move things to use vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Fragment } from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// components
+import InputTransaction from './components/InputTrans'
+import ListTransactions from './components/ListTrans'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Fragment>
+      <div className='container'>
+        <InputTransaction />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <ListTransactions />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Fragment>
   )
 }
 
---
+++ client/src/components/InputTrans.jsx
@@ -0,0 +1,42 @@
+import React, { Fragment, useState } from 'react'
+
+const InputTransaction = () => {
+  const [transaction_date, setTransaction_date] = useState('')
+  const [item, setItem] = useState('')
+  const [price, setPrice] = useState('')
+
+  const onSubmitForm = async e => {
+    e.preventDefault()
+    try {
+      const body = { transaction_date, item, price }
+      await fetch('http://localhost:5000/transactions', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+      setTransaction_date('')
+      setItem('')
+      setPrice('')
+      window.location = '/'
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Expanse Tracker List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input type="date" className="form-control  m-2" value={transaction_date}
+          onChange={e => setTransaction_date(e.target.value)}/>
+        <input type="text" className="form-control m-2" value={item}
+         onChange={e => setItem(e.target.value)} placeholder="Item"/>
+        <input type="text" className="form-control m-2" value={price}
+         onChange={e => setPrice(e.target.value)} placeholder="Price"/>
+        <button className="btn btn-success w-75 h-85 m-2">Add</button>
+      </form>
+    </Fragment>
+  )
+}
+
+export default InputTransaction
