576d843ebeff182e9100f4cdd0d8c957b4776654
Lucas D. Bauer <lucassoccer95@gmail.com>
2023-08-21 12:01:03 -0500

    Add navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import { Fragment } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './App.css'
 
 // components
+import Home from './components/home'
 import Transaction from './components/transaction/Transaction'
+import ListBudget from './components/budget/ListBudget'
+import TopNavBar from './components/nav/NavBar';
 
 function App() {
   return (
-    <Fragment>
-      <div className='container'>
-        <Transaction />
+      <div className='App'>
+        <Router>
+          <TopNavBar />
+            <Routes>
+                <Route exact path='/' element={<Home />}/>
+                <Route path='/transaction' element={<Transaction />}/>
+                <Route path='/budget' element={<ListBudget />}/>
+            </Routes>
+        </Router>
       </div>
-    </Fragment>
   )
 }
 
--- client/src/components/nav/NavBar.jsx
+++ client/src/components/nav/NavBar.jsx
@@ -1,33 +1,22 @@
-import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react"
+import Container from 'react-bootstrap/Container'
+import { Nav, Navbar, NavLink } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const TopNavBar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/budget" to='/budget'>Home</Nav.Link>
-            <Nav.Link href="/Transaction" to="/transaction">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Container>
+      <Navbar.Brand href='/'>React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link eventKey="1" href='/'>Home</Nav.Link>
+          <Nav.Link eventKey="3" href="/budget">Budget</Nav.Link>
+          <Nav.Link eventKey="2" href="/transaction">Transactions</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
     </Navbar>
   )
 }
