0e61989afcdc28f558e93f7887467e655b5e51d2
Lucas Bauer <lucassoccer95@gmail.com>
2023-08-28 14:03:24 -0500

    Add rule for camelCase

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -17,6 +17,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "react/prop-types": [0]
+    "react/prop-types": [0],
+    "camelcase": [
+      "off"
+  ]
   },
 }
